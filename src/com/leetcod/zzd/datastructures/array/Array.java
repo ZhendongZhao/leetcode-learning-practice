a76971a46ff1798f93f8b3556310297d2408773e
zhaozhendong <zhaozhendong1026@163.com>
2020-07-21 15:19:41 +0800

    搜索插入位置

--- src/com/leetcod/zzd/datastructures/array/Array.java
+++ src/com/leetcod/zzd/datastructures/array/Array.java
@@ -68,6 +68,7 @@ public class Array {
 
 
     /**
+     * 题目：搜索插入位置
      * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
      *
      * 你可以假设数组中无重复元素。
@@ -90,9 +91,18 @@ public class Array {
      * 输出: 0
      */
     public int searchInsert(int[] nums, int target) {
-        return 0;
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
-
-
 
 }
