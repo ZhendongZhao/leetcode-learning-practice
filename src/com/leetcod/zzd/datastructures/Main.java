31fe76f8b1995c0cfa3cf6a6976204cde26dc629
zhaozhendong <zhaozhendong1026@163.com>
2020-07-21 14:25:34 +0800

    Initial commit

---
+++ src/com/leetcod/zzd/datastructures/Main.java
@@ -0,0 +1,9 @@
+package com.leetcod.zzd.datastructures;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hhhh");
+    }
+}
