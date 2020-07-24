9c0b74b9b15ec00994f61c8606d9016c39b6e16d
zhaozhendong <zhaozhendong1026@163.com>
2020-07-24 14:17:18 +0800

    合并区间

--- src/com/leetcod/zzd/datastructures/array/Array.java
+++ src/com/leetcod/zzd/datastructures/array/Array.java
@@ -1,5 +1,9 @@
 package com.leetcod.zzd.datastructures.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 数组
  *
@@ -103,6 +107,88 @@ public class Array {
             }
         }
         return ans;
+    }
+
+    /**
+     * 题目：合并区间
+     * 给出一个区间的集合，请合并所有重叠的区间。
+     *
+     * 示例 1:
+     *
+     * 输入: [[1,3],[2,6],[8,10],[15,18]]
+     * 输出: [[1,6],[8,10],[15,18]]
+     * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+     * 示例 2:
+     *
+     * 输入: [[1,4],[4,5]]
+     * 输出: [[1,5]]
+     * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     */
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals.length == 0 || intervals == null) {
+            return res.toArray(new int[0][]);
+        }
+        // 对起点终点进行排序
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int i = 0;
+        while (i < intervals.length) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            // 如果有重叠，循环判断哪个起点满足条件
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                i++;
+                right = Math.max(right, intervals[i][1]);
+            }
+            // 将现在的区间放进res里面
+            res.add(new int[]{left, right});
+            // 接着判断下一个区间
+            i++;
+        }
+        return res.toArray(new int[0][]);
+    }
+
+    /**
+     * 题目：旋转矩阵
+     * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+     *
+     * 不占用额外内存空间能否做到？
+     *
+     * 示例 1:
+     *
+     * 给定 matrix =
+     * [
+     *   [1,2,3],
+     *   [4,5,6],
+     *   [7,8,9]
+     * ],
+     *
+     * 原地旋转输入矩阵，使其变为:
+     * [
+     *   [7,4,1],
+     *   [8,5,2],
+     *   [9,6,3]
+     * ]
+     * 示例 2:
+     *
+     * 给定 matrix =
+     * [
+     *   [ 5, 1, 9,11],
+     *   [ 2, 4, 8,10],
+     *   [13, 3, 6, 7],
+     *   [15,14,12,16]
+     * ],
+     *
+     * 原地旋转输入矩阵，使其变为:
+     * [
+     *   [15,13, 2, 5],
+     *   [14, 3, 4, 1],
+     *   [12, 6, 8, 9],
+     *   [16, 7,10,11]
+     * ]
+     */
+    public void rotate(int[][] matrix) {
+
     }
 
 }
