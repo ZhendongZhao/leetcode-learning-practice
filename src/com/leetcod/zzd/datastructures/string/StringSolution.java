df7e8f5d338b7dc26e71fa69e3f97fbd100082be
zhaozhendong <zhaozhendong1026@163.com>
2020-10-30 13:55:05 +0800

    移动零

--- src/com/leetcod/zzd/datastructures/string/StringSolution.java
+++ src/com/leetcod/zzd/datastructures/string/StringSolution.java
@@ -544,4 +544,52 @@ public class StringSolution {
         return slow + 1;
     }
 
+    /**
+     * 移动零
+     * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     *
+     * 示例:
+     *
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     * 说明:
+     *
+     * 必须在原数组上操作，不能拷贝额外的数组。
+     * 尽量减少操作次数。
+     *
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 0) {
+            return;
+        }
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        while (i < nums.length) {
+            nums[i++] = 0;
+        }
+    }
+
+    /**
+     * 一次遍历 快速排序
+     * 首先要确定一个待分割的元素做中间点0，然后把所有不等于0的元素放到0的左边，等于0的元素放到其右边
+     */
+    public void moveZeroes1(int[] nums) {
+        if (nums.length == 0) {
+            return;
+        }
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i++] = temp;
+            }
+        }
+    }
+
 }
