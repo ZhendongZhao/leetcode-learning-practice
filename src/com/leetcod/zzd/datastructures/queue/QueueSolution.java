726748b7797abf4b7c9feced348d939d25e13788
zhaozhendong <zhaozhendong1026@163.com>
2020-11-09 14:54:54 +0800

    岛屿数量

---
+++ src/com/leetcod/zzd/datastructures/queue/QueueSolution.java
@@ -0,0 +1,86 @@
+package com.leetcod.zzd.datastructures.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: zhendong.Z
+ * @date: 2020-11-04 10:58 上午
+ */
+public class QueueSolution {
+
+    /**
+     * 队列是典型的 FIFO 数据结构。插入（insert）操作也称作入队（enqueue），新元素始终被添加在队列的末尾。
+     *    删除（delete）操作也被称为出队（dequeue)。 你只能移除第一个元素。
+     *
+     * 广度优先搜索（BFS）是一种遍历或搜索数据结构（如树或图）的算法。一个常见应用是找出从根结点到目标结点的最短路径。
+     */
+
+    /**
+     * 输入：grid = [
+     *   ["1","1","1","1","0"],
+     *   ["1","1","0","1","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","0","0","0"]
+     * ]
+     * 输出：1
+     * 示例 2：
+     *
+     * 输入：grid = [
+     *   ["1","1","0","0","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","1","0","0"],
+     *   ["0","0","0","1","1"]
+     * ]
+     * 输出：3
+     *  
+     *
+     * 提示：
+     *
+     * m == grid.length
+     * n == grid[i].length
+     * 1 <= m, n <= 300
+     * grid[i][j] 的值为 '0' 或 '1'
+     *
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return num_islands;
+    }
+}
