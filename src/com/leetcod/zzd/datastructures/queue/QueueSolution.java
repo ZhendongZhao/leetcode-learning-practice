6bb4316f68e88e8a6cb084c61a75b4f8fdd944ea
zhaozhendong <zhaozhendong1026@163.com>
2020-11-11 16:35:36 +0800

    完全平方数

--- src/com/leetcod/zzd/datastructures/queue/QueueSolution.java
+++ src/com/leetcod/zzd/datastructures/queue/QueueSolution.java
@@ -1,5 +1,6 @@
 package com.leetcod.zzd.datastructures.queue;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -168,5 +169,52 @@ public class QueueSolution {
             }
         }
         return -1;
+    }
+
+    /**
+     * 完全平方数
+     * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+     *
+     * 示例 1:
+     *
+     * 输入: n = 12
+     * 输出: 3
+     * 解释: 12 = 4 + 4 + 4.
+     * 示例 2:
+     *
+     * 输入: n = 13
+     * 输出: 2
+     * 解释: 13 = 4 + 9.
+     *
+     */
+    public int numSquares(int n) {
+        //TODO 好好复习
+        ArrayList<Integer> square_nums = new ArrayList<Integer>();
+        for (int i = 1; i * i <= n; ++i) {
+            square_nums.add(i * i);
+        }
+
+        Set<Integer> queue = new HashSet<Integer>();
+        queue.add(n);
+
+        int level = 0;
+        while (queue.size() > 0) {
+            level += 1;
+            Set<Integer> next_queue = new HashSet<Integer>();
+
+            for (Integer remainder : queue) {
+                for (Integer square : square_nums) {
+                    if (remainder.equals(square)) {
+                        return level;
+                    } else if (remainder < square) {
+                        break;
+                    } else {
+                        next_queue.add(remainder - square);
+                    }
+                }
+            }
+            queue = next_queue;
+        }
+        return level;
     }
 }
