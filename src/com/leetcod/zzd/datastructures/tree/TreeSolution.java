8007893c13cf6631c6a9c074ac1029e542663300
zhaozhendong <zhaozhendong1026@163.com>
2020-11-16 16:08:07 +0800

    二叉树的中序遍历

--- src/com/leetcod/zzd/datastructures/tree/TreeSolution.java
+++ src/com/leetcod/zzd/datastructures/tree/TreeSolution.java
@@ -109,4 +109,65 @@ public class TreeSolution {
         return res;
     }
 
+    /**
+     * 二叉树的中序遍历
+     * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+     * 示例 1：
+     * 输入：root = [1,null,2,3]
+     * 输出：[1,3,2]
+     *
+     * 示例 2：
+     * 输入：root = []
+     * 输出：[]
+     *
+     * 示例 3：
+     * 输入：root = [1]
+     * 输出：[1]
+     *
+     * 示例 4：
+     * 输入：root = [1,2]
+     * 输出：[2,1]
+     *
+     * 示例 5：
+     * 输入：root = [1,null,2]
+     * 输出：[1,2]
+     *
+     * 提示：
+     * 树中节点数目在范围 [0, 100] 内
+     * -100 <= Node.val <= 100
+     *
+     * 思路：递归
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+    /**
+     * 栈
+     */
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
 }
