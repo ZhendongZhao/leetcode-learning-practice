8b9d6d23b61d79b21215f066e9e3fc1b16b848fc
zhaozhendong <zhaozhendong1026@163.com>
2020-11-19 12:34:32 +0800

    对称二叉树

--- src/com/leetcod/zzd/datastructures/tree/TreeSolution.java
+++ src/com/leetcod/zzd/datastructures/tree/TreeSolution.java
@@ -308,4 +308,46 @@ public class TreeSolution {
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    /**
+     * 对称二叉树
+     * 给定一个二叉树，检查它是否是镜像对称的。
+     *
+     *  
+     *
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     *  
+     *
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     *
+     *  思路：递归
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        return recur(root.left, root.right);
+    }
+
+    private boolean recur(TreeNode left, TreeNode right){
+        if (left == null && right == null){
+            return true;
+        }
+        if (left == null || right ==null || left.val != right.val){
+            return false;
+        }
+        return recur(left.left, right.right) && recur(left.right, right.left);
+    }
+
 }
