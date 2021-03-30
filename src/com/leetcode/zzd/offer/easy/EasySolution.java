ad1e69b584168af228c35bc5c97c7a5b6d704efd
zhaozhendong <zhaozhendong1026@163.com>
2021-03-30 19:25:41 +0800

    替换空格

--- src/com/leetcode/zzd/offer/easy/EasySolution.java
+++ src/com/leetcode/zzd/offer/easy/EasySolution.java
@@ -51,6 +51,14 @@ public class EasySolution {
      * 0 <= s 的长度 <= 10000
      */
     public String replaceSpace(String s) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
