538e0cd33259756e71b9ee2c11713a188a4414f1
zhaozhendong <zhaozhendong1026@163.com>
2021-06-23 20:31:34 +0800

    合并两个排序的链表

--- src/com/leetcode/zzd/offer/easy/EasySolution.java
+++ src/com/leetcode/zzd/offer/easy/EasySolution.java
@@ -321,15 +321,48 @@ public class EasySolution {
     public ListNode getKthFromEnd(ListNode head, int k) {
         ListNode fast = head;
         ListNode slow = head;
-        for (int i = 0; i<k;i++){
+        for (int i = 0; i < k; i++) {
             fast = fast.next;
         }
-        while (fast != null){
+        while (fast != null) {
             fast = fast.next;
             slow = slow.next;
         }
         return slow;
     }
+
+
+    /**
+     * 合并两个排序的链表
+     *
+     * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+     * 示例1：
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     *
+     * 限制：
+     * 0 <= 链表长度 <= 1000
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0);
+        ListNode cur = dum;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+
+    /**
+     * 二叉树的镜像
+     */
 
 
 
