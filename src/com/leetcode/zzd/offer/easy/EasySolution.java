cfdbb7605fe660be2ff301db47481e59101ff2f5
zhaozhendong <zhaozhendong1026@163.com>
2021-06-23 16:30:30 +0800

    链表中倒数第k个节点

--- src/com/leetcode/zzd/offer/easy/EasySolution.java
+++ src/com/leetcode/zzd/offer/easy/EasySolution.java
@@ -1,7 +1,6 @@
 package com.leetcode.zzd.offer.easy;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
@@ -307,6 +306,30 @@ public class EasySolution {
         return result;
     }
 
+    /**
+     * 链表中倒数第k个节点
+     * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+     *
+     * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+     *
+     * 示例：
+     *
+     * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+     *
+     * 返回链表 4->5.
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i<k;i++){
+            fast = fast.next;
+        }
+        while (fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
 
 
 
