cc0e8f18f3c02ba3072edbe0dd6dfbb9cae1a366
zhaozhendong <zhaozhendong1026@163.com>
2021-06-24 10:33:12 +0800

    连续子数组的最大和

--- src/com/leetcode/zzd/offer/easy/EasySolution.java
+++ src/com/leetcode/zzd/offer/easy/EasySolution.java
@@ -490,6 +490,32 @@ public class EasySolution {
     }
 
 
+    /**
+     * 连续子数组的最大和
+     * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+     * 要求时间复杂度为O(n)。
+     *
+     * 示例1:
+     * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释:   连续子数组   [4,-1,2,1] 的和最大，为   6。
+     *
+     * 提示：
+     * 1 <=   arr.length <= 10^5
+     * -100 <= arr[i] <= 100
+     *
+     *思路：动态规划，最优解
+     */
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+
 
 
 }
