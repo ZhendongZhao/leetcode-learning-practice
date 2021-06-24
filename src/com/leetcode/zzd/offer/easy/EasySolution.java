e6ffab08d9cdafdc5274cdbe5edb7a2a4d92ee5b
zhaozhendong <zhaozhendong1026@163.com>
2021-06-24 15:49:20 +0800

    从上到下打印二叉树 II

--- src/com/leetcode/zzd/offer/easy/EasySolution.java
+++ src/com/leetcode/zzd/offer/easy/EasySolution.java
@@ -1,7 +1,11 @@
 package com.leetcode.zzd.offer.easy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
@@ -530,6 +534,53 @@ public class EasySolution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length/2];
+    }
+
+    /**
+     * 从上到下打印二叉树 II
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     *
+     * 例如:
+     * 给定二叉树: [3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回其层次遍历结果：
+     *
+     * [
+     *   [3],
+     *   [9,20],
+     *   [15,7]
+     * ]
+     *
+     * 提示：
+     * 节点总数 <= 1000
+     * 思路： BFS广度优先
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
     }
 
 
